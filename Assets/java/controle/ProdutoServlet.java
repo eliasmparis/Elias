c7010ef19318accc23e03026db1c0c849bb81df4
eliasmparis <eliasmparis2013@gmail.com>
2022-11-03 04:32:10 -0300

    Create ProdutoServlet.java
    criar um formulario de java para banco de dados

---
+++ Assets/java/controle/ProdutoServlet.java
@@ -0,0 +1,121 @@
+package controle;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import modelo.Produto;
+
+/**
+ *
+ * @author Elias
+ */
+@WebServlet(urlPatterns = {"/ProdutoServlet"})
+public class ProdutoServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet ProdutoServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet ProdutoServlet at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            
+            
+            String acao = request.getParameter("acao");
+            
+            if (acao != null && acao.equals("excluir")) {
+                Produto prod = new Produto();
+                prod.setNome(request.getParameter("nome"));
+                prod.excluir();
+                request.setAttribute("msg", "Produto foi exlcuido com sucesso!!!!");
+                RequestDispatcher despachar = request.getRequestDispatcher("products.jsp");
+                despachar.forward(request, response);  
+            } else {
+                if (acao != null && acao.equals("inserir")) {
+                    RequestDispatcher despachar = request.getRequestDispatcher("products.jsp");
+                    despachar.forward(request, response);  
+                } else {
+                    Produto prod = new Produto();;
+                    prod.setNome(request.getParameter("selecao"));
+                    prod.consultarPorNome();
+                    
+                    request.setAttribute("produto", prod);
+                    RequestDispatcher despachar = request.getRequestDispatcher("products.jsp");
+                    despachar.forward(request, response);  
+                }
+            }
+                        
+        } catch (Exception e) {
+            request.setAttribute("msg", e.getMessage());
+            RequestDispatcher despachar = request.getRequestDispatcher("erros.jsp");
+            despachar.forward(request, response);  
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
